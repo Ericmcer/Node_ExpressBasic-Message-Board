77ae55ba28fe130cee5acb2d9f7f8662e5ddfa20
Eric McElyea <emcelyea@horizon.csueastbay.edu>
2014-09-29 16:02:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var db = mongo.db('mongodb://localhost:27017/messages', {native_parser:"true"})
 
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -31,7 +31,7 @@ app.use(function(req,res,next){
 })
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
